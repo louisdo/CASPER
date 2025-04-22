daaf950bd6187eaa8fd98c3c2881ebf9bf9172df
dungnguyen <nguyenmanhdzung5920@gmail.com>
2025-04-22 02:25:20 -0500

    add kpeval

---
+++ evaluation.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+export PYTHONIOENCODING=utf-8
+
+# List of JSON files to process
+files=(
+  # "inspec--phrase_splade_33.json"
+  # "krapivin--phrase_splade_33.json"
+  # "nus--phrase_splade_33.json"
+  # "semeval--phrase_splade_33.json"
+  # # "kp20k--phrase_splade_33.json"
+  "semeval--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty.json"
+  "inspec--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty.json"
+  "nus--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty.json"
+  "krapivin--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty.json"
+  # # "kp20k--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty.json"
+  "semeval--autokeygen-1.json"
+  "inspec--autokeygen-1.json"
+  "nus--autokeygen-1.json"
+  "krapivin--autokeygen-1.json"
+  # # "kp20k--autokeygen-1.json"
+  "semeval--copyrnn-1.json"
+  "inspec--copyrnn-1.json"
+  "nus--copyrnn-1.json"
+  "krapivin--copyrnn-1.json"
+  # "kp20k--copyrnn-1.json"
+  # "semeval--uokg-1.json"
+  # "inspec--uokg-1.json"
+  # "nus--uokg-1.json"
+  # "krapivin--uokg-1.json"
+  # "kp20k--uokg-1.json"
+  # "semeval--tpg-1.json"
+  # "inspec--tpg-1.json"
+  # "inspec--tpg-2.json"
+  # "inspec--tpg-3.json"
+  # "kp20k--tpg-2.json"
+  # "krapivin--tpg-2.json"
+  # "krapivin--tpg-3.json"
+  # "nus--tpg-2.json"
+  # "nus--tpg-3.json"
+  # "semeval--tpg-2.json"
+  # "semeval--tpg-3.json"
+  # "semeval--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_10.json"
+  # "inspec--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_10.json"
+  # "nus--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_10.json"
+  # "krapivin--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_10.json"
+  # "kp20k--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_10.json"
+  # "semeval--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_50.json"
+  # "inspec--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_50.json"
+  # "nus--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_50.json"
+  # "krapivin--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_50.json"
+  # "kp20k--retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-1_position_penalty+length_penalty_neighborsize_50.json"
+  # "semeval--embedrank_sentence_transformers_all-MiniLM-L12-v2.json"
+  # "inspec--embedrank_sentence_transformers_all-MiniLM-L12-v2.json"
+  # "nus--embedrank_sentence_transformers_all-MiniLM-L12-v2.json"
+  # "krapivin--embedrank_sentence_transformers_all-MiniLM-L12-v2.json"
+  # "kp20k--embedrank_sentence_transformers_all-MiniLM-L12-v2.json"
+)
+
+files=(
+  "inspec--phrase_splade_33.json"
+  "krapivin--phrase_splade_33.json"
+  "nus--phrase_splade_33.json"
+  "semeval--phrase_splade_33.json"
+)
+
+# Process each file
+for file in "${files[@]}"; do
+  echo "Processing $file..."
+
+  # input_file="/scratch/lamdo/phrase_splade_keyphrase_generation_results/$file"
+  # input_file="/scratch/lamdo/keyphrase_generation_results/results_ongoing/$file" \
+
+
+  input_file="/scratch/lamdo/phrase_splade_keyphrase_generation_results/$file" \
+  output_dir="_gitig_samples/" \
+  top_k=10 \
+  python metrics/convert_splade_file.py
+
+  python evaluation.py \
+    --config-file /home/lvnguyen/research/Efficient-Storage-for-Large-IR-Systems/kpeval/config.gin \
+    --jsonl-file "_gitig_samples/_all_keyphrase_$file" \
+    --metrics diversity,meteor,approximate_matching,exact_matching,rouge,bert_score,semantic_matching,unieval \
+    --log-file-prefix _gitig_results/
+done
---
+++ metrics/convert_splade_file.py
@@ -0,0 +1,81 @@
+import json 
+import os 
+from datasets import load_dataset
+
+# def convert_file_(file, output = "_gitig_samples/"):
+#     data = json.load(open(file)) 
+#     present_keyphrases_data = []
+#     absent_keyphrases_data = []
+
+#     for idx, item in enumerate(data): 
+#         present_keyphrases_data.append(
+#             {
+#                 "id": idx, 
+#                 "source": " ; ".join(item['present_keyphrases'] + item['automatically_extracted_keyphrases']['present_keyphrases']), 
+#                 "target": item['present_keyphrases'] + item['absent_keyphrases'], 
+#                 "predictions": item['automatically_extracted_keyphrases']['present_keyphrases'] + item['automatically_extracted_keyphrases']['absent_keyphrases']
+#             }
+#         )
+
+#         absent_keyphrases_data.append(
+#             {
+#                 "id": idx, 
+#                 "source": " ; ".join(item['present_keyphrases'] + item['automatically_extracted_keyphrases']['present_keyphrases']), 
+#                 "target": item['absent_keyphrases'], 
+#                 "predictions": item['automatically_extracted_keyphrases']['absent_keyphrases']
+#             }
+#         )
+    
+#     with open(f"{output}_present_keyphrase_{file.split('/')[-1]}", 'w') as f:
+#         f.write(json.dumps(present_keyphrases_data))
+#     with open(f"{output}_absent_keyphrase_{file.split('/')[-1]}", 'w') as f:
+#         f.write(json.dumps(absent_keyphrases_data))
+#     return present_keyphrases_data, absent_keyphrases_data
+
+def download_kp20k(): 
+    dataset = load_dataset("midas/kp20k", "generation", split="test")
+    return [" ".join(dataset[idx]['document']) for idx in range(len(dataset))]
+
+def download_all_source(file): 
+    file_name = file.split('/')[-1]
+    dataset_type = file_name.split('--')[0]
+    if dataset_type == "kp20k": 
+        return download_kp20k()
+    
+    dataset = load_dataset(f"memray/{dataset_type}") 
+    all_titles = list(dataset['test']['title'])
+    all_abstracts = list(dataset['test']['abstract'])
+
+    sources = [f"{title} . {abstract}" for title, abstract in zip(all_titles, all_abstracts)]
+
+    return sources
+
+
+def convert_file_(file, output = "_gitig_samples/", top_k = 10):
+    data = json.load(open(file)) 
+    sources = download_all_source(file)
+    assert len(data) == len(sources)
+
+    all_keyphrases_data = []
+
+    for idx, item in enumerate(data): 
+        all_keyphrases_data.append(
+            {
+                "id": idx, 
+                "source": sources[idx], 
+                "target": item['present_keyphrases'] + item['absent_keyphrases'], 
+                "predictions": item['automatically_extracted_keyphrases']['present_keyphrases'][:top_k] + item['automatically_extracted_keyphrases']['absent_keyphrases'][:top_k]
+            }
+        )
+
+    
+    with open(f"{output}_all_keyphrase_{file.split('/')[-1]}", 'w') as f:
+        f.write(json.dumps(all_keyphrases_data, indent=4))
+
+
+
+if __name__ == "__main__": 
+    input_file = os.environ['input_file']
+    output_dir = os.environ['output_dir']
+    top_k = int(os.environ['top_k'])
+    convert_file_(input_file, output_dir, top_k)
